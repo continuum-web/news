6c145b1b2843dce96568b31ef0b5f03383e995cd
Scott Wears <scottwears@Voyager.fritz.box>
2021-11-01 12:31:04 +0000

    tests comment data is returned in the correct format

--- db/utils.js
+++ db/utils.js
@@ -1,12 +1,18 @@
 exports.formatDataForEntry = data => {
 	const formattedData = data.map(element => {
 		const dataArray = [];
-		for (const [key, value] of Object.entries(element)) {
-			dataArray.push(value);
+        for (const [key, value] of Object.entries(element)) {
+            if (key == 'created_at') {
+                dataArray.push(String(value));
+            }
+            else {
+                dataArray.push(value)
+            }
+			
 		}
 
 		return dataArray;
 	});
-
+    console.log(formattedData)
 	return formattedData;
 };

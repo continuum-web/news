fb762d8e2b2ea9d5461657493714cf4b31089a79
Scott Wears <scottwears@Voyager.fritz.box>
2021-11-01 12:34:37 +0000

    Tests user Data is returned correctly

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -282,7 +282,7 @@ describe('tests the format utility function', () => {
 			];
 			expect(formatDataForEntry(commentData)).toEqual(output);
         });
-        it.only('should return the array topic data in the array in array format', () => {
+        it('should return the array topic data in the array in array format', () => {
 			const output = [
 				['The man, the Mitch, the legend', 'mitch'],
 				['Not dogs', 'cats'],
@@ -290,6 +290,31 @@ describe('tests the format utility function', () => {
 			];
 ;
 			expect(formatDataForEntry(topicData)).toEqual(output);
+        });
+        it('should return the array user data in the array in array format', () => {
+			const output = [
+				[
+					'butter_bridge',
+					'jonny',
+					'https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg',
+				],
+				[
+					'icellusedkars',
+					'sam',
+					'https://avatars2.githubusercontent.com/u/24604688?s=460&v=4',
+				],
+				[
+					'rogersop',
+					'paul',
+					'https://avatars2.githubusercontent.com/u/24394918?s=400&v=4',
+				],
+				[
+					'lurker',
+					'do_nothing',
+					'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
+				],
+			];
+			expect(formatDataForEntry(userData)).toEqual(output);
 		});
 	});
 });

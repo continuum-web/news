424c94246452e38f445d383b37c98d6779bbd48c
Scott Wears <scottwears1@outlook.com>
2021-11-05 11:14:42 +0000

    Responds with endpoints.json when requesting /api/

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,10 +2,18 @@ const apiRouter = require('express').Router()
 const topicsRouter = require('./topicsRouter');
 const articlesRouter= require('./articlesRouter')
 const commentsRouter = require('./commentsRouter');
+const fs = require('fs');
 
 //sends the user an welcome message
 apiRouter.get('/', (req, res) => {
-    res.status(200).send({ msg: "Welcome to the API" })
+    const jsonFile = fs.readFile('./endpoints.json', (err, json) => {
+		let obj = JSON.parse(json);
+		res.json(obj);
+    });
+    jsonFile.then((obj) => {
+        res.status(200).send(obj)
+    })
+    
 })
 
 // routers for /topics /comments and /articles
